0463b531fe938735eeaf3c62716ad6dead1407b0
hcui3 <huibincu@gmail.com>
2024-08-31 00:10:07 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,95 @@
+const navMenuBtn = document.querySelector("#nav_menu_btn");
+const navMenu = document.querySelector("#nav_menu");
+
+navMenuBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-is-open");
+});
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 30) {
+    header.classList.add("header-is-active");
+  } else {
+    header.classList.remove("header-is-active");
+  }
+});
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    960: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// scroll reveal
+const sr = ScrollReveal({
+  origin: "bottom",
+  distance: "60px",
+  duration: 3000,
+  delay: 600,
+  reset: true,
+});
+
+// hero
+sr.reveal(".hero__text", { origin: "top" });
+
+// steps
+sr.reveal(".steps__step", { distance: "100px", interval: 100 });
+
+// about
+sr.reveal(".about__text", { origin: "left" });
+sr.reveal(".about__img", { origin: "right", delay: 800 });
+
+// testimonial
+sr.reveal(".testimonial__bg", { delay: 800 });
+sr.reveal(".testimonial__title");
+sr.reveal(".testimonial__slider", { delay: 1000 });
+
+// brands
+sr.reveal(".brands__img", { delay: 600, distance: "100px", interval: 100 });
+
+// work
+sr.reveal(".work__title");
+sr.reveal(".work__subtitle", { delay: 800 });
+sr.reveal(".work__grid", { delay: 1000 });
+
+// stats
+sr.reveal(".stats");
+sr.reveal(".stats__item", {
+  distance: "100px",
+  interval: 100,
+});
+
+// news
+sr.reveal(".news__title");
+sr.reveal(".news__subtitle", { delay: 800 });
+
+sr.reveal(".news__item", {
+  distance: "100px",
+  interval: 100,
+  delay: 1000,
+});
+
+// contact
+sr.reveal(".contact__container");
+sr.reveal(".contact__text", { delay: 800 });
+
+// footer
+sr.reveal(".footer__item", { distance: "100px", interval: 100 });
+sr.reveal(".footer__copyright");
